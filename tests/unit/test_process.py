3a5b9048003d782ef719c02811a4d7e72e586a15
tomcarter <tomcarter017@gmail.com>
2024-12-06 10:52:37 +0000

    data path fixture

--- tests/unit/test_process.py
+++ tests/unit/test_process.py
@@ -18,6 +18,11 @@ class MockMongoCollection:
     insert_one = MagicMock()
 
 
+@pytest.fixture
+def data_path():
+    return "./tests/unit/data/paper_data.json"
+
+
 @pytest.fixture(autouse=True)
 def reset_mocks():
     MockSentenceTransformer.encode.reset_mock()
@@ -30,9 +35,9 @@ def reset_mocks():
 @patch("pymongo.collection.Collection", return_value=MockMongoCollection())
 @patch("faiss.Index", return_value=MockFaissIndex())
 @patch("sentence_transformers.SentenceTransformer", return_value=MockSentenceTransformer())
-def mock_data_handler(mock_sentence_transformer, mock_faiss_index, mock_mongo_collection):
+def mock_data_handler(mock_sentence_transformer, mock_faiss_index, mock_mongo_collection, data_path):
     dh = DataHandler(
-        dataset="./tests/data/paper_data.json",
+        dataset=data_path,
         embedding_model=mock_sentence_transformer(),
         faiss_index=mock_faiss_index(),
         mongo_collection=mock_mongo_collection(),
@@ -40,22 +45,22 @@ def mock_data_handler(mock_sentence_transformer, mock_faiss_index, mock_mongo_co
     return dh
 
 
-def test_data_handler_load_data(mock_data_handler):
-    data = [d for d in mock_data_handler.load_data("./tests/data/paper_data.json")]
+def test_data_handler_load_data(mock_data_handler, data_path):
+    data = [d for d in mock_data_handler.load_data(data_path)]
     assert len(data) == 2
     assert data[0]["title"] == "The Art of Computer Programming"
     assert data[1]["abstract"] == "This is a book about the C programming language"
 
 
-def test_data_handler_load_data_n(mock_data_handler):
-    data = [d for d in mock_data_handler.load_data("./tests/data/paper_data.json", n=1)]
+def test_data_handler_load_data_n(mock_data_handler, data_path):
+    data = [d for d in mock_data_handler.load_data(data_path, n=1)]
     assert len(data) == 1
     assert data[0]["title"] == "The Art of Computer Programming"
 
 
-def test_data_handler_load_data_n0_raises(mock_data_handler):
+def test_data_handler_load_data_n0_raises(mock_data_handler, data_path):
     with pytest.raises(ValueError):
-        _ = [d for d in mock_data_handler.load_data("./tests/data/paper_data.json", n=0)]
+        _ = [d for d in mock_data_handler.load_data(data_path, n=0)]
 
 
 def test_data_handler_embedding_vector(mock_data_handler):
